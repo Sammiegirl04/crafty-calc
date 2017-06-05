6b20eb978a3873db331194faeb90d1e2d9878460
Samantha Tucker <sammiegirl04@gmail.com>
2017-06-05 10:53:53 -0500

    added a js file, tried to add event listener and test statements but all I get is silence! Definitely doing something wrong. Working on js file.

---
+++ calc.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("button").click()(function(){
+    var validAmount = (?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$
+    var x = document.getElementById("cost").value;
+
+    console.log(x)
+    console.log(x.match(validAmount));
+  });
+});
