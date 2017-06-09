30197b88267314f85cc18d1ed3b922c2cf0a04be
Shawn Jones <shawn.greg.jones@gmail.com>
2017-06-09 16:41:01 -0500

    Added some adjustments that would help move in the right direction

--- calc.js
+++ calc.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-  $("button").click()(function(){
-    var validAmount = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/
+  $("button").click(function(){
+    var validAmount = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
     var x = document.getElementById("cost").value;
+    var y = document.getElementById("units").value;
+    var z = document.getElementById("time").value;
+    var iscostValid = validAmount.test(x);
+    var areUnitsValid = // new rgEx
+    var isTimeValid = // new rgEx
 
-    console.log(x)
-    console.log(x.match(validAmount));
+    if(iscostValid && areUnitsValid && isTimeValid){
+    	// move forward with math and return math
+    } else {
+    	// display errors to the user
+    }
   });
 });
