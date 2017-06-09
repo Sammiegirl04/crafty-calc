00f9d98fb6d9a9233b2e9b99bc8be70906bcd037
Samantha Tucker <sammiegirl04@gmail.com>
2017-06-09 16:07:00 -0500

    added script tag and button element

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("button").click()(function(){
-    var validAmount = (?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$
+    var validAmount = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/
     var x = document.getElementById("cost").value;
 
     console.log(x)
