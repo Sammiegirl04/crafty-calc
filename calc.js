c49d47bf65e3b902f3692a671279d341f82f4df3
Samantha Tucker <sammiegirl04@gmail.com>
2017-06-13 13:41:31 -0500

    added time, cost and units variables. Began to formulate the math if/else statement.

--- calc.js
+++ calc.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
   $("button").click(function(){
     var validAmount = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
+    var validUnit = /^[0-9]*$/;
+    var validTime = /^[0-2][0-3]:[0-5][0-9]$/;
     var x = document.getElementById("cost").value;
     var y = document.getElementById("units").value;
     var z = document.getElementById("time").value;
     var iscostValid = validAmount.test(x);
-    var areUnitsValid = // new rgEx
-    var isTimeValid = // new rgEx
+    var areUnitsValid = validAmount.test(y);
+    var isTimeValid = validTime.test(z);
 
-    if(iscostValid && areUnitsValid && isTimeValid){
+    console.log(iscostValid);
+    console.log(areUnitsValid);
+    console.log(isTimeValid);
+
+  //  if(iscostValid && areUnitsValid && isTimeValid){
     	// move forward with math and return math
-    } else {
+  //  } else {
     	// display errors to the user
-    }
+  //  )}
   });
 });
