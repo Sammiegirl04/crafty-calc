88b6c09b1d27237713f314ef57962c7196a825ef
Samantha Tucker <sammiegirl04@gmail.com>
2017-06-21 13:52:34 -0500

    finished arithmetic and added styling with bootstrap.

--- calc.js
+++ calc.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
   $("button").click(function(){
     var validAmount = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
-    var validUnit = /^[0-9]*$/;
+    var validUnit = /^[1-9]*$/;
     var validTime = /^[0-9]*$/;
     var x = document.getElementById("cost").value;
     var y = document.getElementById("units").value;
@@ -19,9 +20,10 @@ $(document).ready(function() {
     if(isCostValid && areUnitsValid && isTimeValid && isLaborValid){
       var costPerUnit = x / y;
       var labCost = z /60 * lab;
-      var price = costPerUnit + labCost * 2;
+      var markup = document.getElementById("markup").value;
+      var price = costPerUnit + labCost * markup;
 
-      document.getElementById("unitcost").innerHTML = 'Your suggested selling price per item is ' + price;
+      document.getElementById("price").innerHTML = 'Your suggested selling price per item is $' + price;
 
   //  } else {
     	// display errors to the user
