0ced25657ad3752268ac584734386c8d976d5397
Samantha Tucker <sammiegirl04@gmail.com>
2017-06-29 00:26:07 -0500

    added error checks, messages and styling.

--- calc.js
+++ calc.js
@@ -2,36 +2,51 @@ $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
 
   $("#reset").click(function(){
-    document.getElementById("myForm").reset();
+    $("input").empty();
   });
 
   $("#button").click(function(){
-    var validAmount = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
-    var validUnit = /^[1-9]*$/;
-    var validTime = /^[0-9]*$/;
+    //var validAmount = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/;
+    var validAmount = /^\$?([1-9]\d{0,2}(,\d{3})*(\.\d{2})?|[1-9]\d*(\.\d{2})?|0?\.(?!00)\d{2})$/
+    var validUnit = /^[0-9]+$/;
+    var validTime = /^[0-9]+$/;
+    var validMarkup = /^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$/
+
     var x = document.getElementById("cost").value;
     var y = document.getElementById("units").value;
     var z = document.getElementById("time").value;
     var lab = document.getElementById("labor").value;
+    var markup = document.getElementById("markup").value;
+
     var isCostValid = validAmount.test(x);
     var areUnitsValid = validUnit.test(y);
     var isTimeValid = validTime.test(z);
     var isLaborValid = validAmount.test(lab);
+    var isMarkupValid = validMarkup.test(markup);
 
-    console.log(isCostValid);
-    console.log(areUnitsValid);
-    console.log(isTimeValid);
-
-    if(isCostValid && areUnitsValid && isTimeValid && isLaborValid){
+    if(isCostValid && areUnitsValid && isTimeValid && isLaborValid && isMarkupValid){
       var costPerUnit = x / y;
       var labCost = z /60 * lab;
-      var markup = document.getElementById("markup").value;
       var price = costPerUnit + labCost * markup;
       var results = price.toFixed(2);
       document.getElementById("price").innerHTML = 'Your suggested selling price per item is: $' + results;
 
-  //  } else {
-
+    } else {
+        if (x === 0 || ! isCostValid){
+          document.getElementById("costError").innerHTML = " Please enter a valid amount";
+      }
+        if (! areUnitsValid || y == 0){
+          document.getElementById("unitError").innerHTML = " Please enter at least 1 unit";
+      }
+        if (! isTimeValid || z == 0){
+          document.getElementById("timeError").innerHTML = " Please enter time in minutes only";
+      }
+        if (! isLaborValid || lab == 0){
+          document.getElementById("labError").innerHTML = " Please enter a valid amount";
+      }
+        if (! isMarkupValid || markup == 0){
+          document.getElementById("markupError").innerHTML = " Please enter a markup of at least 1";
+      };
     };
   });
 });
